c28324f68d9049a66d8477110574120c2948fdfb
mehdi <0mvakilin0@gmail.com>
2024-10-08 19:47:18 +0330

    fix MustJoinChannels class

--- src/Routing/Middlewares/MustJoinChannels.php
+++ src/Routing/Middlewares/MustJoinChannels.php
@@ -4,9 +4,7 @@
 namespace TGMehdi\Routing\Middlewares;
 
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
-use TGMehdi\BotMiddleware;
 use TGMehdi\TelegramBot;
 use TGMehdi\Types\InlineKeyboard;
 use TGMehdi\Types\InlineMessage;
@@ -20,21 +18,21 @@ class MustJoinChannels implements MiddlewareContract
         $this->args = $args;
     }
 
-    public function handle(TelegramBot $telegramBot)
+    public function handle(TelegramBot $bot)
     {
         $args = $this->args;
-        $cache_status = Redis::get($telegramBot->chat_id . '.join.' . $args[0]);
+        $cache_status = Redis::get($bot->chat_id . '.join.' . $args[0]);
         if ($cache_status !== true) {
-            $status = $telegramBot->get_chat_member($args[0], $telegramBot->user["id"]);
+            $status = $bot->get_chat_member($args[0], $bot->data[$bot->get_update_type()]['from']["id"]);
             if (!isset($status["result"]["status"])) return true;
             $status = $status["result"]["status"];
             if ($status == "left" or $status == "kicked") {
                 $keyboard = new InlineKeyboard();
                 $keyboard->newButton($args[0], options: ['url' => "https://t.me/" . substr($args[0], 1)]);
-                $telegramBot->send_text(new InlineMessage($keyboard,__("tgmehdi.messages.you_must_join_channels")));
+                $bot->send_text(new InlineMessage($keyboard, __("tgmehdi.messages.you_must_join_channels")));
                 return false;
             } else {
-                Redis::set($telegramBot->chat_id . '.join.' . $args[0], true, 86400);
+                Redis::set($bot->chat_id . '.join.' . $args[0], true, 86400);
             }
         }
 
