2142e5aaf2422af322e319a5cbb7a5f9881c26b0
mehdi <0mvakilin0@gmail.com>
2025-07-12 11:35:20 +0330

    add management middleware

--- routes/tgmehdi.php
+++ routes/tgmehdi.php
@@ -7,11 +7,18 @@ $r = ['as' => 'tgmehdi.', 'prefix' => config('tgmehdi.url_prefix')];
 if (config('tgmehdi.middleware')) {
     $r['middleware'] = config('tgmehdi.middleware');
 }
+
 Route::group($r, function () {
-    Route::get('/', BotController::class . '@index')->name('index');
-    Route::group(['prefix' => '{bot_name}'], function () {
+
+    $m = [];
+    if (config('tgmehdi.management_middleware')) {
+        $m = config('tgmehdi.management_middleware');
+    }
+    $m = array_merge($m, ['web']);
+    Route::get('/', BotController::class . '@index')->name('index')->middleware($m);
+    Route::group(['prefix' => '{bot_name}'], function () use ($m) {
         Route::post('bot', BotController::class . '@bot')->name('bot');
-        Route::group(['middleware' => ['web']], function () {
+        Route::group(['middleware' => $m], function () {
             Route::get('update', BotController::class . '@local_bot')->name('local_bot');
             Route::get('set_webhook', BotController::class . '@set_webhook')->name('set_webhook');
             Route::get('delete_webhook', BotController::class . '@delete_webhook')->name('delete_webhook');
